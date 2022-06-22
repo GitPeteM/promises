9709b1f4214a64d125c6f917163100c4084e3fbf
Peter McBride <mrpetermcbride@gmail.com>
2022-06-22 12:30:55 -0700

    Halfway - bare_minimum/promiseConstructor.js exercises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -4,18 +4,38 @@
  * to the function invocation, while errors should be available in the `catch` block
  */
 
-var fs = require('fs');
 var request = require('needle');
 var Promise = require('bluebird');
+// var fs = require('fs');
+var fs = require('fs');
+// Promise.promisifyAll(fs);
+
+const readFile = Promise.promisify(fs.readFile);
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+
+  var promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        // console.log(err)
+        reject(err);
+      } else {
+        resolve(data.toString().split('\n')[0]);
+      }
+    });
+  });
+
+  // var promise = readFile(filePath);
+
+  return promise;
 };
+
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
